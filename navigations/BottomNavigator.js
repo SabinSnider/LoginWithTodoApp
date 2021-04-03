b656c169c7b3d1e511430563419752f88daadc88
SabinSnider <sabinvsic@gmail.com>
2021-04-03 20:31:39 +0545

    bottomNav added

--- navigations/BottomNavigator.js
+++ navigations/BottomNavigator.js
@@ -1,22 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
-const BottomNavigator = (pros) => {
+const BottomNavigator = pros => {
   return (
     <Tab.Navigator
-    tabBarOptions={{
-      style: {
-        height: 55,
-        borderTopWidth: 0,
-        elevation: 0,
-      },
-      showLabel: false,
-      activeTintColor: COLORS.primary,
-    }}>
-
-
+      tabBarOptions={{
+        style: {
+          height: 55,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        showLabel: false,
+        activeTintColor: COLORS.primary,
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="home-filled" color={color} size={28} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
